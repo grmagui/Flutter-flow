453dd85d1da43e9ed869281aa27faa324331783c
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2026-03-30 13:35:09 +0000

    Updating to latest FlutterFlow output.

--- lib/formulario/formulario_widget.dart
+++ lib/formulario/formulario_widget.dart
@@ -1,6 +1,7 @@
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import '/flutter_flow/flutter_flow_widgets.dart';
+import '/flutter_flow/custom_functions.dart' as functions;
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -671,33 +672,31 @@ class _FormularioWidgetState extends State<FormularioWidget> {
                                 ),
                                 child: FFButtonWidget(
                                   onPressed: () async {
-                                    if (/* NOT RECOMMENDED */ _model
-                                            .textController2.text ==
-                                        'true') {
-                                      ScaffoldMessenger.of(context)
-                                          .showSnackBar(
-                                        SnackBar(
-                                          content: Text(
-                                            'CPF válido ',
-                                            style: TextStyle(
-                                              color:
-                                                  FlutterFlowTheme.of(context)
-                                                      .primaryText,
-                                            ),
-                                          ),
-                                          duration:
-                                              Duration(milliseconds: 4000),
-                                          backgroundColor:
-                                              FlutterFlowTheme.of(context)
-                                                  .secondary,
-                                        ),
+                                    if (functions.validarCPF(
+                                        _model.textController2.text)) {
+                                      await showDialog(
+                                        context: context,
+                                        builder: (alertDialogContext) {
+                                          return AlertDialog(
+                                            title: Text('ok'),
+                                            content: Text('ok'),
+                                            actions: [
+                                              TextButton(
+                                                onPressed: () => Navigator.pop(
+                                                    alertDialogContext),
+                                                child: Text('Ok'),
+                                              ),
+                                            ],
+                                          );
+                                        },
                                       );
                                     } else {
                                       await showDialog(
                                         context: context,
                                         builder: (alertDialogContext) {
                                           return AlertDialog(
-                                            title: Text('CPF inválido '),
+                                            title: Text('Erro'),
+                                            content: Text('Erro'),
                                             actions: [
                                               TextButton(
                                                 onPressed: () => Navigator.pop(
